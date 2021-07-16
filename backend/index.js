42725515d73bb6df47cda1316f04809a9b55e96b
Abraham Obed <65366889+ofceab@users.noreply.github.com>
2021-07-16 04:24:57 +0000

    UI almost ready

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express =require('express');
+
+//Create app
+const app=express();
+
+app.get('/search:keyword',(res,req)=>{
+    
+});
+
+
+
+app.listen(3000,()=>console.log("The server is running on port : 3000 fir"));
